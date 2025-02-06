ee710a20f8c893492f12db31fc11e174d1e7208b
tory99 <hjw990826@gmail.com>
2025-02-06 17:14:01 +0900

    진짜 완성성

--- index.js
+++
@@ -1,5 +0,0 @@
-let server = require('./server.js');
-let router = require('./router.js');
-let requesthandler = require('./requesthandler.js');
-
-server.start(router.route, requesthandler.handle);
